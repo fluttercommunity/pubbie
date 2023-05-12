a55754e738fbbbfe9b8fce96c9505f9b2c4bbcfb
GroovinChip <groovinchip@gmail.com>
2023-05-12 17:04:31 -0400

    pubbie_cli initial creation

---
+++ pubbie_cli/bin/pubbie_cli.dart
@@ -0,0 +1,5 @@
+import 'package:pubbie_cli/pubbie_cli.dart' as pubbie_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pubbie_cli.calculate()}!');
+}
---
+++ pubbie_cli/test/pubbie_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:pubbie_cli/pubbie_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
